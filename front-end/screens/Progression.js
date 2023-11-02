0fb062d272c71e6e8829f678a4dde25c1658746f
viger-lejarza.audrey <viger-lejarza.audrey@courrier.uqam.ca>
2023-11-02 12:50:18 -0400

    ajout soleil

--- front-end/screens/Progression.js
+++ front-end/screens/Progression.js
@@ -1,30 +1,35 @@
-import React from 'react';
+import React from "react";
 import { StyleSheet, Text, View } from "react-native";
-import Imprimer from '../components/imprimer/print.js'
+import Imprimer from "../components/imprimer/print.js";
 import { StatusBar } from "expo-status-bar";
 import SectionHautComponent from "../components/progression/SectionHautComponent";
-import BoiteEncadree from "../components/progression/Encadre"
-
+import BoiteEncadree from "../components/progression/Encadre";
+import TauxDiffComponent from "../components/progression/TauxDiffComponent.js";
 
 export default class Progression extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Imprimer type={"programme"}/>
-                <Text>Progression</Text>
-                <SectionHautComponent sessionsCompleted={3} totalSessions={5} />
-                <StatusBar style="auto" />
-                <BoiteEncadree gauche={<Text>Element gauche</Text>} droite={<Text>Element droite</Text>}/>
+  render() {
+    return (
+      <View style={styles.container}>
+        <Imprimer type={"programme"} />
+        <SectionHautComponent sessionsCompleted={3} totalSessions={5} />
+        <StatusBar style="auto" />
+        <BoiteEncadree
+          gauche={
+            <View>
+              <Text>Difficulté moyenne</Text>
+              <TauxDiffComponent difficultyLevels={[2]}></TauxDiffComponent>
             </View>
-        );
-    }
-
+          }
+          droite={<Text>Element droite</Text>}
+        />
+      </View>
+    );
+  }
 }
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        justifyContent: "center",
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
 });
-
