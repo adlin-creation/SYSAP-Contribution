11eb3e2af2a17b42b43fbb87e0f72f4665f3cfff
Benjamin Landry <benjaminlandry@Benjamins-MacBook-Air.local>
2023-11-08 19:08:55 -0500

    Ajout liste déroulante de semaines pour bouton semaine

--- front-end/components/progression/SemaineComponent.js
+++ front-end/components/progression/SemaineComponent.js
@@ -1,19 +1,33 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
-import Feather from "react-native-vector-icons/Feather";
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import SelectDropdown from 'react-native-select-dropdown';
 
-
-
-//A MODIFIER POUR FAIRE UNE BARRE DEROULANTE
 const SemaineComponent = ({ semaine }) => {
+  const weeks = [...Array(52).keys()].map(i => `Semaine ${i + 1}`);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.texteSemaine}>Semaine {`${semaine}`} </Text>
-      <Feather
-        style={styles.icon}
-        name="chevron-down"
-        size={30}
-        color={"white"}
+      <SelectDropdown
+        data={weeks}
+        defaultValueByIndex={semaine - 1} // assuming semaine is 1-based index
+        onSelect={(selectedItem, index) => {
+          console.log(selectedItem, index);
+        }}
+        buttonTextAfterSelection={(selectedItem, index) => {
+          return selectedItem;
+        }}
+        rowTextForSelection={(item, index) => {
+          return item;
+        }}
+        buttonStyle={styles.dropdownButton}
+        buttonTextStyle={styles.dropdownButtonText}
+        renderDropdownIcon={() => {
+          return <Feather name="chevron-down" size={20} color={"white"} />;
+        }}
+        dropdownIconPosition={'right'}
+        dropdownStyle={styles.dropdownStyle}
+        rowTextStyle={styles.rowTextStyle} // Add this line
       />
     </View>
   );
@@ -21,22 +35,40 @@ const SemaineComponent = ({ semaine }) => {
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "purple",
+    backgroundColor: 'purple',
     paddingLeft: 30,
     paddingRight: 15,
     paddingVertical: 3,
     margin: 10,
     borderRadius: 20,
-    alignItems: "center",
-    flexDirection: "row",
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+    height: 42,
   },
-  texteSemaine: {
-    color: "white",
+  dropdownButton: {
+    backgroundColor: 'transparent',
+    width: '100%',
+    height: '100%',
+    alignSelf: 'flex-start',
+  },
+  dropdownButtonText: {
+    color: 'white',
+    textAlign: 'left',
     fontSize: 15,
-    fontWeight: "bold",
+    fontWeight: 'bold',
   },
-  icon: {
-    marginLeft: 5,
+  dropdownStyle: {
+    backgroundColor: 'purple',
+    borderRadius: 20,
+    marginTop: Platform.OS === 'android' ? -50 : 0,
+  },
+  rowStyle: {
+    paddingVertical: 10,
+  },
+  rowTextStyle: {
+    color: 'white',
   },
 });
 
