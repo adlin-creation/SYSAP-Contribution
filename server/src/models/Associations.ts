9b49f3d82ce5b3be45911131eaa66c508757dde3
Jeremy Asselin <asselin.jeremy@courrier.uqam.ca>
2023-11-15 09:53:21 -0500

    merge

---
+++ server/src/models/Associations.ts
@@ -0,0 +1,94 @@
+import Exercise from './Exercise';
+import ExerciseRecord from './ExerciseRecord';
+import ExerciseSeries from './ExerciseSeries';
+import ExerciseSeriesExercise from './ExerciseSeriesExercise';
+import Program from './Program';
+import ProgramDayRecord from './ProgramDayRecord';
+import Patient from './Patient';
+import ProgramExerciseSeries from './ProgramExerciseSeries';
+import Reminder from './Reminder';
+
+export function createAssociations(){
+    Program.hasMany(ProgramExerciseSeries, {
+        foreignKey: 'ProgramName',
+        as: 'ExerciseSeries',
+    });
+    ProgramExerciseSeries.belongsTo(Program, {
+        foreignKey: 'ProgramName',
+        as: 'Program',
+    });
+      
+
+    ExerciseSeries.hasMany(ExerciseSeriesExercise, {
+        foreignKey: 'ExerciseSeriesId',
+        as: 'Exercises',
+    });
+    ExerciseSeriesExercise.belongsTo(ExerciseSeries, {
+        foreignKey: 'ExerciseSeriesId',
+        as: 'ExerciseSeries',
+    });
+      
+
+    ProgramExerciseSeries.belongsTo(ExerciseSeries, {
+        foreignKey: 'ExerciseSeriesId',
+        as: 'Series',
+    });
+    ExerciseSeries.hasMany(ProgramExerciseSeries, {
+        foreignKey: 'ExerciseSeriesId',
+        as: 'Programs',
+    });
+      
+    ExerciseSeriesExercise.belongsTo(Exercise, {
+        foreignKey: 'ExerciseId',
+        as: 'Exercise',
+    });
+    Exercise.hasMany(ExerciseSeriesExercise, {
+        foreignKey: 'ExerciseId',
+        as: 'SeriesExercises',
+    });
+      
+    Program.hasMany(Patient, {
+        foreignKey: 'ProgramName',
+        as: 'Patients',
+    });
+    Patient.belongsTo(Program, {
+        foreignKey: 'ProgramName',
+        as: 'Program',
+    });
+
+    Patient.hasMany(Reminder, {
+        foreignKey: 'PatientId',
+        as: 'Reminders',
+    });
+    Reminder.belongsTo(Patient, {
+        foreignKey: 'PatientId',
+        as: 'Patient',
+    });
+    
+    Patient.hasMany(ProgramDayRecord, {
+        foreignKey: 'PatientId',
+        as: 'DayRecords',
+    });
+    ProgramDayRecord.belongsTo(Patient, {
+        foreignKey: 'PatientId',
+        as: 'Patient',
+    });
+      
+    ProgramDayRecord.hasMany(ExerciseRecord, {
+        foreignKey: 'ProgramDayRecordId',
+        as: 'ExerciseRecords',
+    });
+    ExerciseRecord.belongsTo(ProgramDayRecord, {
+        foreignKey: 'ProgramDayRecordId',
+        as: 'ProgramDayRecord',
+    });
+
+    Exercise.hasMany(ExerciseRecord, {
+        foreignKey: 'ExerciseId',
+        as: 'ExerciseRecords',
+    });
+    ExerciseRecord.belongsTo(Exercise, {
+        foreignKey: 'ExerciseId',
+        as: 'Exercise',
+    });
+}
