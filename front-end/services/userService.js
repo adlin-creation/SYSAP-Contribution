a4f5b08a85ca1ee678251cec84b20d201862cd2f
DGagnon333 <derick.gag46@hotmail.com>
2023-12-07 09:47:29 -0500

    Corrige getUserToken pour mobile

--- front-end/services/userService.js
+++ front-end/services/userService.js
@@ -1,16 +1,20 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { jwtDecode } from 'jwt-decode';
 
 export async function getUserFromToken() {
   try {
     const token = await AsyncStorage.getItem('userToken');
 
     if (token) {
-      const secretKey = 'SuperSecret';
+      var base64Url = token.split('.')[1];
+      var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+      var tokenData = decodeURIComponent(window.atob(base64).split('').map((c) => {
+          return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+      }).join(''));
+      const user = JSON.parse(tokenData).user;
 
-      const decoded = jwtDecode(token, secretKey);
+      console.log(user);
 
-      return decoded.user;
+      return user;
     } else {
       console.error('User token not found');
       return null;
