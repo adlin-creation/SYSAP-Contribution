e16dcd7579eeb3a416a919e0468e0153bc1c6ee1
djawad <ha191952@ens.uqam.ca>
2025-03-05 00:55:35 -0500

    Mise a jour du modele Patient

--- rxapa-hcp-server/src/model/Patient.ts
+++ rxapa-hcp-server/src/model/Patient.ts
@@ -1,5 +1,4 @@
 import { sequelize, dataTypes } from "../util/database";
-
 /**
  * Creates a patient model, which translates to a table in the database.
  * Each attribute of this model represents a column in the table.
@@ -49,4 +48,24 @@ export const Patient = sequelize.define("Patient", {
     allowNull: false,
     defaultValue: 0,
   },
+  numberOfCaregivers: {
+    type: dataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0,
+  },
+  weight: {
+    type: dataTypes.FLOAT,
+    allowNull: true, // Optionnel
+  },
+  weightUnit: {
+    type: dataTypes.ENUM('kg', 'lbs'),
+    allowNull: true, // Optionnel
+  },
+  unikPassHashed: {
+    type: dataTypes.INTEGER,
+    allowNull: true,
+  },
+
 });
+
+
