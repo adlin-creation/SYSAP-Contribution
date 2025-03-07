fdf87b5704757a56c2fead7a3a962d1ba9dfd4e4
Kafile, Sami <kafile.sami@courrier.uqam.ca>
2025-03-06 20:02:36 -0500

    Correction de l'importation et protection de la route logout

--- rxapa-hcp-server/src/routes/UserRoutes.ts
+++ rxapa-hcp-server/src/routes/UserRoutes.ts
@@ -1,11 +1,17 @@
-const express = require("express");
+/* Routes de base pour l'inscription, la connexion et la déconnexion. */
+import express from "express";
+import * as userController from "../controller/UserController"; 
+import  isAuth from "../middleware/isAuth"; 
 
 const router = express.Router();
 
-const userController = require("../controller/UserController");
-
+// Inscription d'un utilisateur
 router.post("/signup", userController.signup);
+
+// Connexion d'un utilisateur et génération d'un token JWT
 router.post("/login", userController.login);
-router.post("/logout", userController.logout);
+
+// Déconnexion protégée par authentification
+router.post("/logout", isAuth, userController.logout);
 
 export default router;
