ceab9833deed7ff11490631e63dbf2816f1acffd
farooq pc <farooq.ch@hotmail.com>
2025-03-09 16:39:52 -0400

    reverting changes to models

--- rxapa-hcp-server/src/model/SessionRecord.ts
+++ rxapa-hcp-server/src/model/SessionRecord.ts
@@ -35,8 +35,4 @@ export const SessionRecord = sequelize.define("SessionRecord", {
     type: dataTypes.DATE,
     allowNull: false,
   },
-  ProgramEnrollementId: {
-    type: dataTypes.INTEGER,
-    allowNull: false,
-  }
 });
