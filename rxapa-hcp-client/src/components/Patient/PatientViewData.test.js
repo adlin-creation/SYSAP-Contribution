82318d53595ec329d868b69a1b0253875fa6592f
David Mai <hh691066@ens.uqam.ca>
2025-03-21 16:01:24 -0400

    test corriger pour utiliser les cle du traduction

--- rxapa-hcp-client/src/components/Patient/PatientViewData.test.js
+++ rxapa-hcp-client/src/components/Patient/PatientViewData.test.js
@@ -1,5 +1,5 @@
 // PatientData.test.js
-import { act } from "react";
+import {act} from "react";
 import '@testing-library/jest-dom';
 import { render, screen, fireEvent, waitFor, within } from "@testing-library/react";
 import PatientViewPage from "./PatientViewPage";
@@ -45,8 +45,8 @@ jest.mock("react-i18next", () => ({
     useTranslation: () => ({
       t: (key) => key, // Retourne simplement la clé de traduction au lieu d'une vraie traduction
     }),
-  }));
-  
+}));
+
 describe("PatientViewPage Component", () => {
     beforeEach(() => {
         // Mock de useQuery pour retourner les sessions
@@ -59,7 +59,7 @@ describe("PatientViewPage Component", () => {
 
     it("renders the component without crashing", () => {
         render(<PatientViewPage patient={mockPatient} onClose={() => { }} />);
-        expect(screen.getByText("Patient Data")).toBeInTheDocument();
+        expect(screen.getByText("Patients:patient_data")).toBeInTheDocument();
     });
 
     it("displays patient information correctly", () => {
@@ -74,16 +74,16 @@ describe("PatientViewPage Component", () => {
     it("displays session data correctly", () => {
         render(<PatientViewPage patient={mockPatient} onClose={() => { }} />);
 
-        const sessionElement = screen.getByText("Session:").closest("p");
+        const sessionElement = screen.getByText("Patients:session").closest("p");
         expect(within(sessionElement).getByText("2")).toBeInTheDocument();
         expect(screen.getByText("2024-03-05")).toBeInTheDocument();
 
-        const sessionInfo = screen.getByText("Session:").closest(".session-info");
+        const sessionInfo = screen.getByText("Patients:session").closest(".session-info");
 
         expect(
             within(sessionInfo).getByText((content, element) => {
                 return (
-                    element.textContent.includes("Difficulty Level:") &&
+                    element.textContent.includes("Patients:difficulty_level") &&
                     element.textContent.includes("3")
                 );
             })
@@ -91,7 +91,7 @@ describe("PatientViewPage Component", () => {
         expect(
             within(sessionInfo).getByText((content, element) => {
                 return (
-                    element.textContent.includes("Pain Level:") &&
+                    element.textContent.includes("Patients:pain_level") &&
                     element.textContent.includes("5")
                 );
             })
@@ -100,7 +100,7 @@ describe("PatientViewPage Component", () => {
         expect(
             within(sessionInfo).getByText((content, element) => {
                 return (
-                    element.textContent.includes("Accomplished Exercice:") &&
+                    element.textContent.includes("Patients:accomplished_exercise") &&
                     element.textContent.includes("4")
                 );
             })
@@ -111,36 +111,36 @@ describe("PatientViewPage Component", () => {
         render(<PatientViewPage patient={mockPatient} onClose={() => { }} />);
 
         // Vérifiez que la dernière session est affichée par défaut
-        let sessionElement = screen.getByText("Session:").closest("p");
+        let sessionElement = screen.getByText("Patients:session").closest("p");
         expect(within(sessionElement).getByText("2")).toBeInTheDocument();
         // Cliquez sur "Previous"
-        fireEvent.click(screen.getByText("Previous"));
+        fireEvent.click(screen.getByText("Patients:button_previous"));
 
-        sessionElement = screen.getByText("Session:").closest("p");
+        sessionElement = screen.getByText("Patients:session").closest("p");
         expect(within(sessionElement).getByText("1")).toBeInTheDocument();
         expect(screen.getByText("2024-03-01")).toBeInTheDocument();
 
-        const sessionInfo = screen.getByText("Session:").closest(".session-info");
+        const sessionInfo = screen.getByText("Patients:session").closest(".session-info");
         const difficultyLevelElement = within(sessionInfo)
-            .getByText("Difficulty Level:")
+            .getByText("Patients:difficulty_level")
             .closest("p");
         expect(difficultyLevelElement).toHaveTextContent("3");
 
         // Use a more specific query for "Pain Level: 5"
         const painLevelElement = within(sessionInfo)
-            .getByText("Pain Level:")
+            .getByText("Patients:pain_level")
             .closest("p");
         expect(painLevelElement).toHaveTextContent("5");
 
         // Use a more specific query for "Accomplished Exercice: 4"
         const accomplishedExerciseElement = within(sessionInfo)
-            .getByText("Accomplished Exercice:")
+            .getByText("Patients:accomplished_exercise")
             .closest("p");
         expect(accomplishedExerciseElement).toHaveTextContent("4");
 
         // Cliquez sur "Next"
-        fireEvent.click(screen.getByText("Next"));
-        sessionElement = screen.getByText("Session:").closest("p");
+        fireEvent.click(screen.getByText("Patients:button_next"));
+        sessionElement = screen.getByText("Patients:session").closest("p");
         expect(within(sessionElement).getByText("2")).toBeInTheDocument();
     });
 
