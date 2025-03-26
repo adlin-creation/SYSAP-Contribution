b24f93dd3aa11a657a9e4086e8b29bc133be1bd8
Kossiwa Dziwodona Senayah <senayah.kossiwa_dziwodona@courrier.uqam.ca>
2025-03-26 00:40:27 -0400

    https://gitlab.info.uqam.ca/rx-apa/sysap/-/tree/42-auth-2-c-securite-protection-contre-les-attaques reduction du temps de blocage de 15 a 1 min dans le fichier middleware rateLimiter.ts

--- rxapa-hcp-server/src/middleware/rateLimiter.ts
+++ rxapa-hcp-server/src/middleware/rateLimiter.ts
@@ -1,7 +1,7 @@
 import rateLimit from "express-rate-limit";
 
 export const loginLimiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 1 * 60 * 1000, // 15 minutes
   max: 3, // 3 tentatives
   message: {
     message: "Trop de tentatives de connexion. Réessayez dans 15 minutes.",
