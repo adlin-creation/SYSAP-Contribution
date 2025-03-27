b74708ba5c9341b66a4d95152618ab010f7897c3
Kossiwa Dziwodona Senayah <senayah.kossiwa_dziwodona@courrier.uqam.ca>
2025-03-26 23:48:07 -0400

    https://gitlab.info.uqam.ca/rx-apa/sysap/-/tree/46-auth-5-a-backend-reinitialisation-du-mot-de-passe?ref_type=heads mise a jour de database.ts pour la mise a jour des colonnes ajoute au model professional_user

--- rxapa-hcp-server/src/util/database.ts
+++ rxapa-hcp-server/src/util/database.ts
@@ -59,7 +59,7 @@ export async function initDatabase() {
      * .sync({ alter: true }) - This checks what is the current state of the table in the database (which columns it has, what are their data types, etc),
      * and then performs the necessary changes in the table to make it match the model.
      */
-    await sequelize.sync();
+    await sequelize.sync({ alter: true });
     console.log("CONNECTED TO THE DATABASE");
     isDBConnected = true;
   } catch (error) {
