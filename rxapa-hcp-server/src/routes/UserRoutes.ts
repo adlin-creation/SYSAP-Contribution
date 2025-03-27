6535a2649737003470e16a2866ec77d7e714e34b
Kossiwa Dziwodona Senayah <senayah.kossiwa_dziwodona@courrier.uqam.ca>
2025-03-26 23:12:57 -0400

    https://gitlab.info.uqam.ca/rx-apa/sysap/-/tree/46-auth-5-a-backend-reinitialisation-du-mot-de-passe?ref_type=heads ajout du middleware validateResetToken pour sécuriser la route POST /auth/reset-password

--- rxapa-hcp-server/src/routes/UserRoutes.ts
+++ rxapa-hcp-server/src/routes/UserRoutes.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { loginLimiter } from "../middleware/rateLimiter"; // Ajout
 import csrf from "csurf";
+import { validateResetToken } from "../middleware/validateResetToken";
 
 const router = express.Router();
 const userController = require("../controller/UserController");
@@ -17,4 +18,8 @@ router.get("/csrf-token", csrfProtection, (req, res) => {
     return res.json({ csrfToken: req.csrfToken() }); // on renvoie le token CSRF
   });
 
+// routes pour le reset de mot de passe
+router.post("/reset-password-request", userController.resetPasswordRequest);
+router.post("/reset-password", userController.resetPassword);
+router.post("/reset-password", validateResetToken, userController.resetPassword);
 export default router;
