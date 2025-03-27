2cf533c68f8928b38679728e078baf985c96e1ae
Kossiwa Dziwodona Senayah <senayah.kossiwa_dziwodona@courrier.uqam.ca>
2025-03-26 20:07:12 -0400

    https://gitlab.info.uqam.ca/rx-apa/sysap/-/tree/42-auth-2-c-securite-protection-contre-les-attaques mise a jour du message d'affichage dans le fichier middleware rateLimiter.ts

--- rxapa-hcp-server/src/middleware/rateLimiter.ts
+++ rxapa-hcp-server/src/middleware/rateLimiter.ts
@@ -1,10 +1,10 @@
 import rateLimit from "express-rate-limit";
 
 export const loginLimiter = rateLimit({
-  windowMs: 1 * 60 * 1000, // 15 minutes
+  windowMs: 1 * 60 * 1000, // 1 minutes
   max: 3, // 3 tentatives
   message: {
-    message: "Trop de tentatives de connexion. Réessayez dans 15 minutes.",
+    message: "Trop de tentatives de connexion. Réessayez dans 1 minutes.",
   },
   standardHeaders: true,
   legacyHeaders: false,
