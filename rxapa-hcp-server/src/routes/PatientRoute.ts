f7d3e9b0c245f8742445fb81cb6ed9bfe99fa6dc
Abdel Nasser MAIGA <82781037+Nasser07-git@users.noreply.github.com>
2025-03-28 00:23:07 -0400

    Fix the issue with patient listing

--- rxapa-hcp-server/src/routes/PatientRoute.ts
+++ rxapa-hcp-server/src/routes/PatientRoute.ts
@@ -8,18 +8,10 @@ router.post("/create-patient", isAuth, patientController.createPatient);
 router.post("/add-caregiver/:id", isAuth, patientController.addCaregiver);
 router.put("/update-patient/:id", isAuth, patientController.updatePatient);
 router.delete("/delete-patient/:id", isAuth, patientController.deletePatient);
+router.get("/patients", isAuth, patientController.getPatients);
 router.get("/patient/:id", isAuth, patientController.getPatient);
 router.get("/patientDetails/:id", isAuth, patientController.getPatientDetails);
-router.get("/patients", isAuth, patientController.getPatients);
-router.get(
-  "/patient/:id/sessions",
-  isAuth,
-  patientController.getPatientSessions
-);
-router.put(
-  "/update-patient-with-caregivers/:id",
-  isAuth,
-  patientController.updatePatientWithCaregivers
-);
+router.get("/patient/:id/sessions", isAuth, patientController.getPatientSessions);
+router.put("/update-patient-with-caregivers/:id", isAuth, patientController.updatePatientWithCaregivers);
 
 export default router;
