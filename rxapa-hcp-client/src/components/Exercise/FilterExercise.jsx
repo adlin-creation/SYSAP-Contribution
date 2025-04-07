02fa04b7a1687b4b969ad2c7e1599de7a23e379e
Majda Bouhou <bh691951@ens.uqam.ca>
2025-04-07 01:04:10 -0400

    Changement du css pour adapter les filtres

--- rxapa-hcp-client/src/components/Exercise/FilterExercise.jsx
+++ rxapa-hcp-client/src/components/Exercise/FilterExercise.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Row, Col, Select, Input } from "antd";
+import { Select, Input } from "antd";
 import { useTranslation } from "react-i18next";
 import { SearchOutlined } from "@ant-design/icons";
 
@@ -25,7 +25,7 @@ export default function FilterExercise({ updateSelectedValues }) {
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
-    updateSelectedValues(e.target.value, "searchTerm"); // Envoie le mot-clé au parent
+    updateSelectedValues(e.target.value, "searchTerm");
   };
 
   const handleStatusChange = (newValue) => {
@@ -34,74 +34,70 @@ export default function FilterExercise({ updateSelectedValues }) {
   };
 
   return (
-    <div className="filter-container">
-      <Row gutter={[16, 16]}>
-        <Col span={8}>
-          <div className="filter-item">
-            <Input
-              placeholder={t("placeholder_search")}
-              value={searchTerm}
-              onChange={handleSearchChange}
-              allowClear
-              prefix={<SearchOutlined />}
-            />
-          </div>
-        </Col>
-        <Col span={8}>
-          <div className="filter-item">
-            <Select
-              value={selectedCategory}
-              onChange={handleCategoryChange}
-              className="select-wide"
-              placeholder={t("placeholder_category")}
-              allowClear
-            >
-              <Option value="ALL">{t("option_all")}</Option>
-              <Option value="Aérobique">{t("option_aerobic")}</Option>
-              <Option value="Endurance">{t("option_endurance")}</Option>
-              <Option value="Force">{t("option_strength")}</Option>
-              <Option value="Flexibilité">{t("option_flexibility")}</Option>
-              <Option value="Équilibre">{t("option_balance")}</Option>
-            </Select>
-          </div>
-        </Col>
-        <Col span={8}>
-          <div className="filter-item">
-            <Select
-              value={selectedFitnessLevel}
-              onChange={handleFitnessLevelChange}
-              className="select-wide"
-              placeholder={t("fitness_level_placeholder")}
-              allowClear
-            >
-              <Option value="ALL">{t("option_all")}</Option>
-              <Option value="Facile">{t("option_easy")}</Option>
-              <Option value="Intermédiaire">{t("option_intermediate")}</Option>
-              <Option value="Avancé">{t("option_advanced")}</Option>
-            </Select>
-          </div>
-        </Col>
-        <Col span={8}>
-          <div className="filter-item">
-            <Select
-              value={selectedStatus}
-              onChange={handleStatusChange}
-              placeholder={t("placeholder_exercise_status")}
-              allowClear
-              className="select-wide"
-            >
-              <Option value="ALL">{t("all")}</Option>
-              <Option value="active">{t("option_active")}</Option>
-              <Option value="inactive">{t("option_inactive")}</Option>
-            </Select>
-          </div>
-        </Col>
-      </Row>
+    <div className="filters-container">
+      <div className="filter-item">
+        <Input
+          placeholder={t("placeholder_search")}
+          value={searchTerm}
+          onChange={handleSearchChange}
+          allowClear
+          prefix={<SearchOutlined />}
+          style={{ minWidth: 200 }}
+        />
+      </div>
+
+      <div className="filter-item">
+        <Select
+          value={selectedCategory}
+          onChange={handleCategoryChange}
+          className="select-wide"
+          placeholder={t("placeholder_category")}
+          allowClear
+          style={{ minWidth: 180 }}
+        >
+          <Option value="ALL">{t("option_all")}</Option>
+          <Option value="Aérobique">{t("option_aerobic")}</Option>
+          <Option value="Endurance">{t("option_endurance")}</Option>
+          <Option value="Force">{t("option_strength")}</Option>
+          <Option value="Flexibilité">{t("option_flexibility")}</Option>
+          <Option value="Équilibre">{t("option_balance")}</Option>
+        </Select>
+      </div>
+
+      <div className="filter-item">
+        <Select
+          value={selectedFitnessLevel}
+          onChange={handleFitnessLevelChange}
+          className="select-wide"
+          placeholder={t("fitness_level_placeholder")}
+          allowClear
+          style={{ minWidth: 180 }}
+        >
+          <Option value="ALL">{t("option_all")}</Option>
+          <Option value="Facile">{t("option_easy")}</Option>
+          <Option value="Intermédiaire">{t("option_intermediate")}</Option>
+          <Option value="Avancé">{t("option_advanced")}</Option>
+        </Select>
+      </div>
+
+      <div className="filter-item">
+        <Select
+          value={selectedStatus}
+          onChange={handleStatusChange}
+          placeholder={t("placeholder_exercise_status")}
+          allowClear
+          className="select-wide"
+          style={{ minWidth: 180 }}
+        >
+          <Option value="ALL">{t("all")}</Option>
+          <Option value="active">{t("option_active")}</Option>
+          <Option value="inactive">{t("option_inactive")}</Option>
+        </Select>
+      </div>
     </div>
   );
 }
 
-// Définition des PropTypes
 FilterExercise.propTypes = {
   updateSelectedValues: PropTypes.func.isRequired,
 };
